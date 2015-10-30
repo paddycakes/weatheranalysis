7d8433cef9ef3b990f12006b1fd57ff4d70cf579
Patrick Gallagher <paddy.gallagher@gmail.com>
2015-10-30 23:58:06 +0000

    Initial project commit with sub-folders for implementations using Java 8 and Clojure

---
+++ weatheranalysis-clojure/test/weatheranalysis_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns weatheranalysis-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [weatheranalysis-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
